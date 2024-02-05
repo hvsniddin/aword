585a8b33ec802105f96aba99743c6ca9116747fc
Husniddin Ravshanov <ravshanovhusniddin.2006@gmail.com>
2024-02-05 21:37:42 +0500

    disabled cors

--- config/settings.py
+++ config/settings.py
@@ -42,9 +42,9 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5500', 'http://127.0.0.1:8001', 'https://aword-i38p.onrender.com', 'https://hvsniddin.pythonanywhere.com']
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:5500', 'http://127.0.0.1:8001', 'https://aword-i38p.onrender.com', 'https://hvsniddin.pythonanywhere.com']
+# CORS_ALLOW_ALL_ORIGINS = True
+# # CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5500', 'http://127.0.0.1:8001', 'https://aword-i38p.onrender.com', 'https://hvsniddin.pythonanywhere.com']
+# CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:5500', 'http://127.0.0.1:8001', 'https://aword-i38p.onrender.com', 'https://hvsniddin.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
+    # 'corsheaders',
     'account',
     'game'
 ]
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
