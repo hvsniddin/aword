ddafb67f6c0f1429f0477c433f75f372984c3556
Husniddin Ravshanov <ravshanovhusniddin.2006@gmail.com>
2024-02-05 21:28:30 +0500

    allowed cors all origins

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,8 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
-CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5500', 'http://127.0.0.1:8001', 'https://aword-i38p.onrender.com', 'https://hvsniddin.pythonanywhere.com']
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5500', 'http://127.0.0.1:8001', 'https://aword-i38p.onrender.com', 'https://hvsniddin.pythonanywhere.com']
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:5500', 'http://127.0.0.1:8001', 'https://aword-i38p.onrender.com', 'https://hvsniddin.pythonanywhere.com']
 
 INSTALLED_APPS = [
