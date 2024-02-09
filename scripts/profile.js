12609d492491a69e9323de3196a90810d757e72e
Husniddin Ravshanov <ravshanovhusniddin.2006@gmail.com>
2024-02-09 12:15:26 +0500

    Implemented autoreload after coming back

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,6 +1,16 @@
 const tabs = document.querySelectorAll(".tabs div");
 const fields = document.querySelectorAll(".field");
 
+window.addEventListener("pageshow", function (event) {
+    var historyTraversal = event.persisted || 
+      (typeof window.performance != "undefined" && 
+      window.performance.navigation.type === 2);
+    if (historyTraversal) {
+      // Handle page restore.
+      window.location.reload();
+    }
+});
+
 tabs.forEach(element => {
     element.addEventListener('click', () => {
         deselectTabs();
